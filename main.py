403a70079ceb5bca6dbd028d34555ffd787adb62
wesley benicio <wesleybenicio4@gmail.com>
2019-11-04 20:18:47 -0300

    v1

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+    Creator : Wesley B. D. S. S.
+"""
+
+"""
+    Etapas Treinamento ::
+    1 - Adicionar a base
+    2 - Pre processamento (Pode personalizar)
+    3 - Treinamento (Pode Personalizar)
+    4 - Uso dados processados por IA
+
+
+    funciona com a seguinte estrutura: 
+
+        lunaChatbot:
+            - data (Onde fica os dados necessários para treinamento)
+            - lib (Onde fica os métodos)
+        main
+"""
+
+
+"""
+ -> aqui deve ter as criações das pastas importantes :
+  - data
+  - model
+  - lib
+  - userCode
+ -> Aqui deve ser gerado todo os códigos utilizado pelo usuário
+"""
+
+
