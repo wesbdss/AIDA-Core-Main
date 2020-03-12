886a76e2eb56db37e241b31e4a5cd45b3843c8b1
wesbdss <wesleybenicio4@gmail.com>
2020-03-12 15:14:02 -0300

    auto commit

--- update.bat
+++ update.bat
@@ -1,31 +1,30 @@
-SET com = "Ajustes nos arquivos de configuração"
 
 cd src/AIDA-fluxo/
 
 git add .
-git commit -m echo %com%
+git commit -m "auto commit"
 git push
 
 cd ../AIDA-preprocessamento-1/
 
 git add .
-git commit -m echo %com%
+git commit -m "auto commit"
 git push
 
 cd ../AIDA-processamento-1/
 
 git add .
-git commit -m echo %com%
+git commit -m "auto commit"
 git push
 
 cd ../AIDA-usercode/
 
 git add .
-git commit -m echo %com%
+git commit -m "auto commit"
 git push
 
 cd ../..
 
 git add .
-git commit -m echo %com%
+git commit -m "auto commit"
 git push
